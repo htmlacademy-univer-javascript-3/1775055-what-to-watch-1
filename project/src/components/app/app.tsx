3a1ae4afc0b8c967f3150f58a39ec0304f930dbd
Daniil <cuharus2002@gmail.com>
2022-09-21 16:24:38 +0500

    Создал компоненты для страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,22 @@
+import StartScreen from '../../pages/StartScreen/StartScreen';
+
+
+const PromoFilmData = {
+  PROMO_FILM_TITLE: 'The Grand Budapest Hotel',
+  PROMO_FILM_GENRE: 'Drama',
+  PROMO_FILM_YEAR: '2014'
+};
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <div>
+      <StartScreen
+        promoFilmTitle = {PromoFilmData.PROMO_FILM_TITLE}
+        promoFilmGenre = {PromoFilmData.PROMO_FILM_GENRE}
+        promoFilmYear = {PromoFilmData.PROMO_FILM_YEAR}
+      />
+    </div>
+  );
 }
 
 export default App;
