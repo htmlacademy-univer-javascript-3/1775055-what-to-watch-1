4126fae7327939ec597d4f737bf40860a50e3973
Daniil <cuharus2002@gmail.com>
2022-09-28 23:24:27 +0500

    Реализовал маршрутизацию

--- project/src/pages/AddReview/AddReview.tsx
+++ project/src/pages/AddReview/AddReview.tsx
@@ -1,4 +1,10 @@
+import {useParams} from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+
 function AddReview() {
+  const params = useParams();
+  // eslint-disable-next-line no-console
+  console.log(params);
   return (
     <section className="film-card film-card--full">
       <div className="film-card__header">
@@ -7,13 +13,7 @@ function AddReview() {
         </div>
         <h1 className="visually-hidden">WTW</h1>
         <header className="page-header">
-          <div className="logo">
-            <a href="main.html" className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+          <Logo/>
           <nav className="breadcrumbs">
             <ul className="breadcrumbs__list">
               <li className="breadcrumbs__item">
--- project/src/pages/Film/Film.tsx
+++ project/src/pages/Film/Film.tsx
@@ -1,5 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
+import {useParams} from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+
 function Film() {
+  const params = useParams();
+  // eslint-disable-next-line no-console
+  console.log(params);
   return (
     <>
       <section className="film-card film-card--full">
@@ -9,13 +15,7 @@ function Film() {
           </div>
           <h1 className="visually-hidden">WTW</h1>
           <header className="page-header film-card__head">
-            <div className="logo">
-              <a href="main.html" className="logo__link">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </a>
-            </div>
+            <Logo/>
             <ul className="user-block">
               <li className="user-block__item">
                 <div className="user-block__avatar">
